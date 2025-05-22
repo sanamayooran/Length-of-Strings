ad986dff93de1acd672202a7a107e9cb31b41af6
sanamayooran <sanamayooran@gmail.com>
2025-05-22 10:02:16 +1000

    Create Len.cs

---
+++ Len.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Length_of_strings
+{
+    internal class Len
+    {
+        public static int[] lens(string[] arr)
+        {
+            if (arr == null)
+            {
+                throw new ArgumentNullException(nameof(arr), "Input array cannot be null.");
+            }
+
+            int[] lengths = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                lengths[i] = arr[i] == null ? 0 : arr[i].Length;
+            }
+            return lengths;
+        }
+    }
+}
